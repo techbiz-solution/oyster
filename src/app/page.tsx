1f6e0a84c04d44bc8674fb4d958223f4bb32b80b
natapon <natapon@vekin.co.th>
2024-10-22 03:58:29 +0700

    change bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@
 // src/app/page.tsx
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
+    <div className="flex items-center justify-center h-screen bg-black-100">
       <h1 className="text-4xl font-bold">Welcome to Oyster App</h1>
     </div>
   );
